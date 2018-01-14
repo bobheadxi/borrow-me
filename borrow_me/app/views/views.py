4c6034fa5f68b9ee4f4aa821da2925224a17b25c
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2018-01-13 21:37:14 -0800

    Added signup page and actual fix for redirect issue

--- borrow_me/app/views/views.py
+++ borrow_me/app/views/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from app.models import Item
 import psycopg2
 import utils
@@ -26,3 +28,17 @@ def additem(request):
     i.save()
 
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
--- borrow_me/borrow_me/urls.py
+++ borrow_me/borrow_me/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from app.views import views as core_views
 
 urlpatterns = [
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^signup/$', core_views.signup, name='signup'),
     url(r'^', include('app.urls')),
 ]
