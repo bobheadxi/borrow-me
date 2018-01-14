40ec65922b0c0efd2cf325b84fa3da695992c76c
Robert Lin <robertlin1@gmail.com>
2018-01-13 20:11:02 -0800

    Auth decorator

--- borrow_me/app/views/views.py
+++ borrow_me/app/views/views.py
@@ -3,9 +3,26 @@ from __future__ import unicode_literals
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from app.models import Item
+import psycopg2
+import utils
 
-def index(request):
-    if request.user.is_authenticated():
-        return render(request, 'registration/profile.html')
-    else:
-        return HttpResponseRedirect('/accounts/login')
+# request.user is the username
+@login_required(login_url='/accounts/login')
+def home(request):
+    '''
+    This is our homepage view. It displays a list of available and relevant items within 10km.
+    '''
+    # TODO : get location from request
+
+    return render(request, 'index.html')
+
+def additem(request):
+    '''
+    This creates a new item.
+    '''
+    i = Item()
+    i.save()
+
+    return render(request, 'index.html')
