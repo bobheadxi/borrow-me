e5ed3a3682458db3b0ca23b11dca9ba5e5b880bb
Robert Lin <robertlin1@gmail.com>
2018-01-13 18:41:54 -0800

    FIx migration of african lions

--- borrow_me/scripts/start-server.sh
+++ borrow_me/scripts/start-server.sh
@@ -3,6 +3,6 @@ echo "Hello world!"
 echo "Waiting for Postgres to boot"
 bash scripts/wait.sh "Postgres" "pg_isready -h db -p 5432"
 
-python manage.py makemigrations app
+python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
