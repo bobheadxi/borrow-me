7939b355a87f2fcc7c454fb6116ac2ae6739bb97
Robert Lin <robertlin1@gmail.com>
2018-01-13 22:10:11 -0800

    Item endpoint setup

--- borrow_me/app/views/views.py
+++ borrow_me/app/views/views.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic import TemplateView, View
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from borrow_me.app.models import Item
 from django.contrib.auth.forms import UserCreationForm
 from app.models import Item
 import psycopg2
@@ -30,6 +33,9 @@ def additem(request):
     return render(request, 'index.html')
 
 def signup(request):
+    '''
+    Signup stuff.
+    '''
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -42,3 +48,41 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+class ItemView(View):
+    '''
+    View for accessing and adding items!
+    '''
+
+    def get(self, request):
+        '''
+        Get items
+        '''
+        items = Item.objects.get()
+        context = {
+            items: items
+        }
+
+        # TODO: get location of user if available
+
+        return render(request, 'index.html', context)
+
+    def post(self, request):
+        '''
+        Add item
+        '''
+        kwargs = dict(request.POST)
+        i = Item(**kwargs)
+        i.save()
+
+        return render(request, 'index.html')
+
+    def put(self, request):
+        '''
+        Modify item
+        '''
+        kwargs = dict(request.PUT)
+        i = Item(**kwargs)
+        i.save()
+
+        return render(request, 'index.html')
