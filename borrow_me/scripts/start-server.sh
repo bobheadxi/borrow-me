24e7e0e9a2ad44db418cdf488001a10f85b03f51
Robert Lin <robertlin1@gmail.com>
2018-01-14 12:10:12 -0800

    Only specify port on startup

--- borrow_me/scripts/start-server.sh
+++ borrow_me/scripts/start-server.sh
@@ -5,4 +5,4 @@ bash scripts/wait.sh "Postgres" "pg_isready -h db -p 5432"
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 8000
