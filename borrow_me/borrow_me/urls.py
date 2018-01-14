44057e89d5c3520acb74f8b8c628a9033dbe30c8
Robert Lin <robertlin1@gmail.com>
2018-01-13 16:16:42 -0800

    Add logout path

--- borrow_me/borrow_me/urls.py
+++ borrow_me/borrow_me/urls.py
@@ -20,5 +20,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
     url(r'^login/', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
+    url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
