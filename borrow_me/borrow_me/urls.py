dbf440e7ca2d4690e9db04f25d004a1be7e15d4b
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2018-01-13 20:44:53 -0800

    Fix part 2

--- borrow_me/borrow_me/urls.py
+++ borrow_me/borrow_me/urls.py
@@ -19,8 +19,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    # url(r'^login/', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
-    # url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('app.urls')),
 ]
